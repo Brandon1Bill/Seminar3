ac19b718a9622ec541b328abebc09cb1eed755db
Nikita <lypa@yandex.ru>
2022-05-12 18:04:50 +0300

    ДЗ

--- Program.cs
+++ Program.cs
@@ -1,33 +1,96 @@
-﻿// 1. Показать вторую цифру трёхзначного числа
-
-
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
 System.Console.WriteLine();
-int number = new Random().Next(100, 1000);
 
-System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+int size = 30;
 
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0;
+    int uneven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+        else uneven++;
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
+    System.Console.WriteLine();
+}
+
+PrintResult(FillArray(size));
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+
+
+
+
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+    System.Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
+    System.Console.WriteLine();
+}
+
+PrintArray(FillArray(size));
+
+
+// 3 Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+System.Console.WriteLine();
+
+
+
+System.Console.WriteLine($"Количетсов элементов массива: {size}");
+
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    System.Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    System.Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    System.Console.WriteLine();
+}
+
+PrintDiffMinMax(FillSourceArray(size));
 System.Console.WriteLine(); 
-
-
-// 2. Показать последнюю цифру трёхзначного числа 
-
-int num = new Random().Next(100, 1000);
-
-System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
-System.Console.WriteLine();
-
-
-// 3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-System.Console.WriteLine();
-
-int dayNumber = new Random().Next(1, 8);
-string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
-string txtResult = "";
-
-if (dayNumber > 5) txtResult = $". Ура! Это выходной день!";
-else txtResult = $", рабочий день.";
-
-System.Console.WriteLine();
-System.Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
-System.Console.WriteLine();
